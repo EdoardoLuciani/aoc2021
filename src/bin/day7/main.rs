ef76cd2ddc00dd1c516eb8a815a17392701d15d7
Edoardo Luciani <edoardo.luciani@gmail.com>
2021-12-09 00:59:44 +0000

    Completed day 7

--- src/bin/day7/main.rs
+++ src/bin/day7/main.rs
@@ -1,3 +1,50 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::BTreeMap;
+
+fn read_values_from_file() -> Vec::<i32> {
+    let mut file = File::open("input.txt").unwrap();
+    let mut line = String::new();
+    file.read_to_string(&mut line);
+    let mut out_vec = Vec::<i32>::new();
+    for num in line.split(",") {
+        out_vec.push(num.parse::<i32>().unwrap());
+    }
+    out_vec
+}
+
+fn compute_cost_for_all_points_part1(nums: Vec::<i32>) -> BTreeMap<u32, u32> {
+    // First value is gonna be the cost and the second is the x coordinate
+    let mut out_map = BTreeMap::new();
+    let max_coordinate = *nums.iter().max().unwrap();
+    for x in 0..=max_coordinate {
+        let mut cost = 0;
+        for num in nums.iter() {
+            cost += (num - x).abs() as u32;
+        }
+        out_map.insert(cost, x as u32);
+    }
+    out_map
+}
+
+fn compute_cost_for_all_points_part2(nums: Vec::<i32>) -> BTreeMap<u32, u32> {
+    // First value is gonna be the cost and the second is the x coordinate
+    let mut out_map = BTreeMap::new();
+    let max_coordinate = *nums.iter().max().unwrap();
+    for x in 0..=max_coordinate {
+        let mut cost = 0;
+        for num in nums.iter() {
+            let distance = (num - x).abs();
+            cost += ((distance)*(distance + 1) / 2) as u32;
+        }
+        out_map.insert(cost, x as u32);
+    }
+    out_map
+}
+
 fn main() {
-    println!("Hello, world");
+    let nums = read_values_from_file();
+    let res1 = compute_cost_for_all_points_part1(nums.clone());
+    let res2 = compute_cost_for_all_points_part2(nums.clone());
+    println!("{:?} {:?}", res1.iter().min(), res2.iter().min());
 }
